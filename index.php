8cc4a93c1d1f12acb231dc93b7156d692641243a
Francesco Pandolfi <francesco.pan@gmail.com>
2020-03-04 17:03:37 +0100

    index.php table

--- index.php
+++ index.php
@@ -20,25 +20,31 @@ include 'database.php'
       <div class="col-12">
 
 
-        <table class="table table-dark">
+        <table class="table table-dark table-bordered">
           <thead>
             <tr>
               <th scope="col">id</th>
               <th scope="col">room_number</th>
-              <th scope="col">floor</th>
-              <th scope="col">beds</th>
+              <th scope="col"></th>
+              <th scope="col"></th>
+              <th scope="col"></th>
             </tr>
           </thead>
           <tbody>
 
-          <?php foreach ($results as $value): ?>
-            <tr>
-              <td><?=$value['id']?></td>
-              <td><?=$value['room_number']?></td>
-              <td><?=$value['floor']?></td>
-              <td><?=$value['beds']?></td>
-            </tr>
-          <?php endforeach ?>
+          
+          <?php  if (!empty($results)) {
+             foreach ($results as $room): ?>
+              <tr>
+                <td><?=$room['id']?></td>
+                <td><?=$room['room_number']?></td>
+                <td><a class="badge badge-primary" href="">VIEW</a></td>
+                <td><a class="badge badge-success" href="">UPDATE</a></td>
+                <td><a class="badge badge-danger" href="">DELETE</a></td>
+              </tr>
+             <?php endforeach; 
+          }?>
+          
 
           </tbody>
         </table>
